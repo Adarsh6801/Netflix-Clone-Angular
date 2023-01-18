7e06a5ae4ca93ed38fe160225601f39cbd286bf8
Adarsh <kpadarsh6801@gmail.com>
2023-01-18 14:33:04 +0530

    categories added

--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -43,4 +43,32 @@ export class MovieApiServiceService {
     getMovieCast(data: any): Observable<any> {
       return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`)
     }
+    // action
+    fetchActionMovies(): Observable<any> {
+      return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=28`);
+    }
+    // adventure
+    fetchAdventureMovies(): Observable<any> {
+      return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=12`);
+    }
+      // animation
+      fetchAnimationMovies(): Observable<any> {
+        return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=16`);
+      }
+        // comedy
+    fetchComedyMovies(): Observable<any> {
+      return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=35`);
+    }
+       // documentary
+       fetchDocumentaryMovies(): Observable<any> {
+        return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=99`);
+      }
+       // Science-fiction
+       fetchScienceFictionMovies(): Observable<any> {
+        return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=878`);
+      }
+       // thriller
+       fetchThrillerMovies(): Observable<any> {
+        return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=53`);
+      }
 }
